a8e0e49195c025df18e718f0e4960ee3bfa0c8b0
Roaaabushaqrah <roaaabushaqrah@gmail.com>
2021-02-10 17:55:03 +0300

    ex1

---
+++ index.js
@@ -0,0 +1,86 @@
+// excersise 1:
+
+// ************
+var a ;
+// ************
+let testContext = 15;
+console.log(testContext);
+// *************
+var a,b,c;
+// ************
+var a ='1',b='2',c='3';
+// ***********
+var success = true;
+var x = false;
+var x = new Boolean(false);
+if (x) {
+  // this code is executed
+}
+// **************
+var arr = new Array();
+// console.log(arr) 
+// **************
+var arr = new Array(1,2,3,4,5,6,7,8,9,10);
+console.log(arr) 
+// **************
+var arr2 = new Array('a','j');
+console.log(arr2)
+// ***************
+const object1 = {
+    a: 'somestring',
+    b: 42,
+    c: false
+  };
+  
+  console.log(Object.keys(object1));
+
+// ***************
+const object2 = {
+    a: 'somestring',
+    b: 42,
+    c: false,
+    array: [1,2,3]
+  };
+  
+  console.log((object2));
+  // ***************
+const object3 = {
+    a: 'somestring',
+    b: 42,
+    c: false,
+    array: [1,2,3],
+    obj :{
+        name:'aseel',
+        job:"fullstack"
+    }
+  };
+  
+  console.log((object2));
+//   *****************
+  let journal = [
+    {events: ["work", "touched tree", "pizza",
+              "running", "television"],
+                 squirrel: false},
+    {events: ["work", "ice cream", "cauliflower",
+              "lasagna", "touched tree", "brushed teeth"],
+               squirrel: false},
+    {events: ["weekend", "cycling", "break", "peanuts",
+              "beer"],
+              squirrel: true},
+   
+  ];
+//   *****************
+var a=10,b=20;
+function sum(a,b){
+  return a+b;
+}
+
+console.log(sum(a,b));
+// **************
+var a=10,b=20;
+function multiply(a,b){
+  return a*b;
+}
+
+console.log(multiply(a,b));
+//
